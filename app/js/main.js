e4b62562fc7de6837736c9c49af3289afad20780
PiKey <p.v.kharytonau@gmail.com>
2024-02-16 21:54:51 +0200

    dev

--- app/js/main.js
+++ app/js/main.js
@@ -1,30 +1,13 @@
+$('.slider').slick({
+    dots: false,
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    speed: 1000,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
 
-$(function() {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() != 0) {
-            $('.header').addClass('not-top');
-        } else if ($(this).scrollTop() === 0) {
-            $('.header').removeClass('not-top');
-        };
-    });
-});
-
-
-
-// Slider script
-
-// Slow motion in menu click
-$(document).ready(function() {
-    $(".menu-item").on("click", "a", function(event) {
-        event.preventDefault();
-        var id = $(this).attr('href'),
-            top = $(id).offset().top,
-            offset = $('header').innerHeight();
-        $('body,html').animate({ scrollTop: top - offset }, 1500);
-        $('*').removeClass('current-item');
-        $(this).addClass('current-item');
-    });
-});
 
 
 // Tabs
@@ -48,14 +31,14 @@ $(document).ready(function() {
 
 
 // Slider for review
-$(function() {
-    $('.section-slider').slick({
-        dots: false,
-        speed: 3000,
-        autoplay: false,
-        cssEase: 'linear',
-        autoplaySpeed: 3000,
-        prevArrow: false,
-        nextArrow: false
-    });
-});
+// $(function() {
+//     $('.slider').slick({
+//         dots: false,
+//         speed: 3000,
+//         autoplay: false,
+//         cssEase: 'linear',
+//         autoplaySpeed: 3000,
+//         prevArrow: false,
+//         nextArrow: false
+//     });
+// });
